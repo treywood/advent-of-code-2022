ba939e2c86b9e22334dc1bc3c5ca4719fcfbb648
Trey Wood <treyw@squareup.com>
2022-12-09 10:20:22 -0700

    day 9

--- day-9/Main.hs
+++ day-9/Main.hs
@@ -1,11 +1,11 @@
 module Main where
 
 import Control.Monad.State
-import Data.List
+import Data.Set (Set, insert, singleton)
 import Utils
 
 type Point = (Int,Int)
-type ParserState = (Point, Point, [(Move,Point,Point)], [Move])
+type ParserState = ([Point], Set Point, [Move])
 
 data Move = Move Char Int deriving (Show)
 instance Read Move where
@@ -18,58 +18,70 @@ origin = (0,0)
 main :: IO ()
 main = run $ do
     input <- getInput
-    let initState = (origin, origin, [], map read $ lines input)
-    let ((), (_,_,ps,_)) = runState process initState
-    _ <- mapM putStrLn $ take 100 (map show ps)
-    return ()
+    let rope = take 10 (repeat origin)
+    let initState = (rope, singleton origin, fmap read $ lines input)
+    let ((), (_,ps,_)) = runState runMoves initState
+    -- _ <- mapM putStrLn $ take 100 (map show ps)
+    return $ length ps
   where
-    process :: State ParserState ()
-    process = state $ process'
+    runMoves :: State ParserState ()
+    runMoves = state $ runMoves'
 
-    process' :: ParserState -> ((), ParserState)
-    process' s@(_,_,_,[]) = ((), s)
-    process' (h,t,ps,move:rest) =
-        let 
-            (h',rest') = processLine h move rest
-            t' = moveTail h' t
-            newState = (h', t', ps ++ [(move,h',t')], rest')
-        in
-            runState process newState
+    runMoves' :: ParserState -> ((), ParserState)
+    runMoves' s@(_,_,[]) = ((), s)
+    runMoves' ([],_,_) = undefined
+    runMoves' ((h:ts), ps, move:rest) = runState runMoves newState
+      where 
+        (h',rest') = moveHead h move rest
+        ts' = moveTails h' ts
+        newState = ((h':ts'), insert (last ts') ps, rest')
 
-    processLine :: Point -> Move -> [Move] -> (Point,[Move])
-    processLine h (Move _ 0) rest = (h, rest)
-    processLine (hx,hy) (Move 'U' 1) rest = ((hx, hy + 1), rest)
-    processLine (hx,hy) (Move 'U' n) rest = ((hx, hy + 1), (Move 'U' (n-1)) : rest)
-    processLine (hx,hy) (Move 'D' 1) rest = ((hx, hy - 1), rest)
-    processLine (hx,hy) (Move 'D' n) rest = ((hx, hy - 1), (Move 'D' (n-1)) : rest)
-    processLine (hx,hy) (Move 'R' 1) rest = ((hx + 1, hy), rest)
-    processLine (hx,hy) (Move 'R' n) rest = ((hx + 1, hy), (Move 'R' (n-1)) : rest)
-    processLine (hx,hy) (Move 'L' 1) rest = ((hx - 1, hy), rest)
-    processLine (hx,hy) (Move 'L' n) rest = ((hx - 1, hy), (Move 'L' (n-1)) : rest)
-    processLine _ _ _ = undefined
+    moveHead :: Point -> Move -> [Move] -> (Point,[Move])
+    moveHead h (Move _ 0) rest = (h, rest)
+    moveHead (hx,hy) (Move 'U' n) rest = ((hx, hy + 1), (Move 'U' (n-1)) : rest)
+    moveHead (hx,hy) (Move 'D' n) rest = ((hx, hy - 1), (Move 'D' (n-1)) : rest)
+    moveHead (hx,hy) (Move 'R' n) rest = ((hx + 1, hy), (Move 'R' (n-1)) : rest)
+    moveHead (hx,hy) (Move 'L' n) rest = ((hx - 1, hy), (Move 'L' (n-1)) : rest)
+    moveHead _ _ _ = undefined
+
+    moveTails :: Point -> [Point] -> [Point]
+    moveTails h ts = moveTails' h ts []
+
+    moveTails' :: Point -> [Point] -> [Point] -> [Point]
+    moveTails' _ [] ts = ts
+    moveTails' h (t:ts) ts' = moveTails' t' ts (ts' ++ [t'])
+      where t' = moveTail h t
 
     moveTail :: Point -> Point -> Point
     moveTail (hx,hy) (tx,ty)
         -- up
-        | hx == tx && dy > 1 = (tx,ty+1)
+        | dx == 0 && dy > 1 = (tx,ty+1)
+
         -- down
-        | hx == tx && dy < -1 = (tx,ty-1)
+        | dx == 0 && dy < -1 = (tx,ty-1)
+
         -- right
-        | hy == ty && dx > 1 = (tx+1,ty)
+        | dy == 0 && dx > 1 = (tx+1,ty)
+
         -- left
-        | hy == ty && dx < -1 = (tx-1,ty)
+        | dy == 0 && dx < -1 = (tx-1,ty)
+
         -- up right
-        | dy == 1 && dx > 1 = (tx+1,ty+1)
-        | dx == 1 && dy > 1 = (tx+1,ty+1)
+        | dy > 0 && dx > 1 = (tx+1,ty+1)
+        | dx > 0 && dy > 1 = (tx+1,ty+1)
+        
         -- up left
-        | dy == 1 && dx < -1 = (tx-1,ty+1)
-        | dx == -1 && dy > 1 = (tx-1,ty+1)
+        | dy > 0 && dx < -1 = (tx-1,ty+1)
+        | dx < 0 && dy > 1 = (tx-1,ty+1)
+
         -- down right
-        | dy == -1 && dx > 1 = (tx+1,ty-1)
-        | dx == 1 && dy > 1 = (tx+1,ty-1)
+        | dy < 0 && dx > 1 = (tx+1,ty-1)
+        | dx > 0 && dy < -1 = (tx+1,ty-1)
+        
         -- down left
-        | dy == -1 && dx < -1 = (tx-1,ty-1)
-        | dx == -1 && dy > 1 = (tx-1,ty-1)
+        | dy < 0 && dx < -1 = (tx-1,ty-1)
+        | dx < 0 && dy < -1 = (tx-1,ty-1)
+
         | otherwise = (tx,ty)
       where
         dx = hx - tx
